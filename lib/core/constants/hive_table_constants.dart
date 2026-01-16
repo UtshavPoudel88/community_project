c98a37a0ad6b41b7e7a42d5952430d01e5296bfc
= <=>
2026-01-16 22:11:39 +0545

    Add core constants

---
+++ lib/core/constants/hive_table_constants.dart
@@ -0,0 +1,8 @@
+class HiveTableConstants{
+  HiveTableConstants._();
+  static const String dbName = 'community_db';
+
+  static const int authTypeId = 0 ;
+static const String authTable = 'auth_table';
+}
+
