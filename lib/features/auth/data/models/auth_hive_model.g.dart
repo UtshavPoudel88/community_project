9a1f9934ae3e6ad1f5811ce4d1515cf4ffe25f1d
= <=>
2026-01-16 22:12:37 +0545

    Add auth API and Hive models

--- lib/features/auth/data/models/auth_hive_model.g.dart
+++ lib/features/auth/data/models/auth_hive_model.g.dart
@@ -1,40 +1,43 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'user_model.dart';
+part of 'auth_hive_model.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
 // **************************************************************************
 
-class UserModelAdapter extends TypeAdapter<UserModel> {
+class AuthHiveModelAdapter extends TypeAdapter<AuthHiveModel> {
   @override
   final int typeId = 0;
 
   @override
-  UserModel read(BinaryReader reader) {
+  AuthHiveModel read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return UserModel(
-      id: fields[0] as String,
-      name: fields[1] as String,
+    return AuthHiveModel(
+      authId: fields[0] as String?,
+      fullName: fields[1] as String,
       email: fields[2] as String,
-      password: fields[3] as String,
+      phoneNumber: fields[3] as String?,
+      password: fields[4] as String?,
     );
   }
 
   @override
-  void write(BinaryWriter writer, UserModel obj) {
+  void write(BinaryWriter writer, AuthHiveModel obj) {
     writer
-      ..writeByte(4)
+      ..writeByte(5)
       ..writeByte(0)
-      ..write(obj.id)
+      ..write(obj.authId)
       ..writeByte(1)
-      ..write(obj.name)
+      ..write(obj.fullName)
       ..writeByte(2)
       ..write(obj.email)
       ..writeByte(3)
+      ..write(obj.phoneNumber)
+      ..writeByte(4)
       ..write(obj.password);
   }
 
@@ -44,7 +47,7 @@ class UserModelAdapter extends TypeAdapter<UserModel> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is UserModelAdapter &&
+      other is AuthHiveModelAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
